fe11ed03e809bafc92765dcf5ce8d3732ae84f98
BIMA <bima46bayu25@gmail..com>
2026-05-02 08:12:41 +0700

    fix: authoritation 3

--- src/lib/api.js
+++ src/lib/api.js
@@ -15,6 +15,9 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
     config.headers['X-Authorization'] = `Bearer ${token}` // Bypass cPanel header stripping
+    
+    // Ultimate fallback for cPanel: append token to URL
+    config.params = { ...config.params, token: token } // Send full token string
   }
 
   return config
